73c1a39c976727741ae5e8c8f3bb26ba65aba0db
VISHAL BANGALE <97726956+vbvishalbangale@users.noreply.github.com>
2023-02-03 23:29:15 +0530

    Add files via upload

---
+++ back/backgroun.py
@@ -0,0 +1,172 @@
+print("----------------------------------------------------------------------------------")
+print("WELCOME HOTELWALA")
+print("----------------------------------------------------------------------------------")
+
+a=input("Enter Your Name : ")
+print("----------------------------------------------------------------------------------")
+
+b =int(input("Choose The Option | 1) Party | 2) Wedding | --> "))
+
+
+if b==1:
+    print("You Choose The Party")
+    print("----------------------------------------------------------------------------------")
+    c=int(input("Enter Your Age : "))
+    if c < 25 :
+        print("Go To Floor No 1")
+        print("----------------------------------------------------------------------------------")
+        e=int(input("Choose the Your night dinner | 1) Veg | 2) Non-Veg | --> "))
+        if e==1:
+            print("You Choose the Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+        elif e==2:
+            print("You Choose the Non-Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+    elif c < 50:
+        print("Go To Floor No 2 ")
+        print("----------------------------------------------------------------------------------")
+        e=int(input("Choose the Your night dinner | 1) Veg | 2) Non-Veg | --> "))
+        if e==1:
+            print("You Choose the Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+        elif e==2:
+            print("You Choose the Non-Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+    elif c < 100:
+        print("Go To Floor No 3 ")
+        print("----------------------------------------------------------------------------------")
+        e=int(input("Choose the Your night dinner | 1) Veg | 2) Non-Veg | --> "))
+        if e==1:
+            print("You Choose the Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+        elif e==2:
+            print("You Choose the Non-Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+                print("----------------------------------------------------------------------------------")
+                f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+        
+        
+        
+elif b==2:
+    print("You Choose The Wedding")
+    print("----------------------------------------------------------------------------------")
+    d=int(input("Enter Your Age : "))
+    if d < 25 :
+        print("Go To Floor No 4")
+        print("----------------------------------------------------------------------------------")
+        e=int(input("Choose the Your night dinner | 1) Veg | 2) Non-Veg | --> "))
+        if e==1:
+            print("You Choose the Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+        elif e==2:
+            print("You Choose the Non-Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+    elif d < 50:
+        print("Go To Floor No 5")
+        print("----------------------------------------------------------------------------------")
+        e=int(input("Choose the Your night dinner | 1) Veg | 2) Non-Veg | --> "))
+        if e==1:
+            print("You Choose the Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+        elif e==2:
+            print("You Choose the Non-Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+    elif d < 100:
+        print("Go To Floor No 6")
+        print("----------------------------------------------------------------------------------")
+        e=int(input("Choose the Your night dinner | 1) Veg | 2) Non-Veg | --> "))
+        if e==1:
+            print("You Choose the Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+        elif e==2:
+            print("You Choose the Non-Veg Dinner ")
+            print("----------------------------------------------------------------------------------")
+            f=int(input("What kind of food you like | 1) Spicy | 2) Medium | -->"))
+            if f==1:
+                print("You Choose Veg Spicy Dinner ")
+            elif f==2:
+                print("You Choose Veg medium Dinner")
+            print("----------------------------------------------------------------------------------")
+            print("Thank You!")
+        
